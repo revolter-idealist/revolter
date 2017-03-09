7d77483f27b139069e2e11c68abef1d8f6786b18
tebaly <support@freedomsex.net>
2017-03-09 13:10:10 +0300

    title

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,12 +8,30 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function getText($dir = '')
+    public function getIdea($idea)
     {
-        $result = '';
-        $file = $this->getParameter('index_file').'.md';
-        $path = $this->getParameter('path.web') . $dir . $file; 
-        if (file_exists($path)) {
+        $result = $this->getParameter('path.idea');
+        if ($idea) {
+            $result = $this->getParameter('path.texts') . '/' . $idea;
+        }
+        return $result. '/';
+    }
+    
+    public function getFile($file)
+    {
+        $result = $this->getParameter('index_file');
+        $default = $this->getParameter('index_name');
+        if ($file and $file != $default) { // $this->getParameter('path.deal') . '/' . 
+            $result = $file;
+        }
+        return $result . '.md';
+    }
+    
+    public function getText($idea = null, $file = null)
+    {
+        $result = '';   
+        $path = $this->getIdea($idea).$this->getFile($file);   
+        if (file_exists($path)) { 
             $content = file_get_contents($path);
             if ($content) {
                 $result = $content;
@@ -22,23 +40,90 @@ class DefaultController extends Controller
         return $result;
     }
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="index")
      */
     public function indexAction(Request $request)
     {
-        return $this->render('default/rev.html.twig', [
-            'content' => $this->getText('/'),
+        return $this->redirectToRoute('file', [
+            'idea' => $this->getParameter('index_idea'),
+            'file' => $this->getParameter('index_name'),
+        ], 301);
+        return $this->render('AppBundle::index.html.twig', [
+            'content' => $this->getText(),
         ]);
     }
     
     /**
-     * @Route("/rev", name="revpage")
+     * @Route("/{idea}/", name="idea")
      */
-    public function revAction(Request $request)
+    public function ideaAction($idea)
     {
-        $content = $this->getText('/');
-        return $this->render('default/rev.html.twig', [
-            'content' => $content,
+        return $this->redirectToRoute('file', [
+            'idea' => $idea,
+            'file' => $this->getParameter('index_name')
+        ], 301);
+    }
+    
+    public function checkTranslate($caption)
+    {
+        $result = false;   
+        $lng = $this->get('translator')->getCatalogue()->all('messages');
+        if (in_array($caption, $lng)) {
+            $lng = array_flip($lng);
+            $result = $lng[$caption];  
+        }
+        return $result;
+    }
+    
+    public function getLinks($idea)
+    {
+        $result = ''; 
+        $path = $this->getIdea($idea).'LINKS.md';
+        if (file_exists($path)) { 
+            $result = file_get_contents($path);
+        }
+        return $result;
+    }
+    
+    public function getMaintain()
+    {
+        $result = ''; 
+        $idea = $this->getParameter('index_idea');
+        $path = $this->getIdea($idea).'MAINTAIN.md';
+        if (file_exists($path)) { 
+            $result = file_get_contents($path);
+        }
+        return $result;
+    }
+    
+    public function getTitle($content)
+    {
+        $str = substr($content, 0, strpos($content, "\n"));
+        $str = preg_replace("~[#*()\[\]_\~`-|=]*~", '', $str);
+        return substr($str, 0, 80);
+    }
+    
+    /**
+     * @Route("/{idea}/{file}", name="file")
+     */
+    public function fileAction($idea, $file)
+    {
+        $content = $this->getText($idea, $file); 
+        if (!$content) {
+            if ($caption = $this->checkTranslate($idea)) {
+                return $this->redirectToRoute('file', [
+                    'idea' => $caption,
+                    'file' => $file
+                ], 301); 
+            }
+            throw $this->createNotFoundException('Страница не найдена');
+        } 
+        $template = $file == 'index' ? 'index' : 'texts';
+        return $this->render('AppBundle::'.$template.'.html.twig', [
+            'title'    => $this->getTitle($content),
+            'content'  => $content,
+            'links'    => $this->getLinks($idea),
+            'maintain' => $this->getMaintain(),
         ]);
     }
 }
